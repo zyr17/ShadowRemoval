21bb580a2aa2fbaab803f4f34ddcdb65c046fef4
zyr17 <jzjqz17@gmail.com>
2017-05-19 13:13:20 +0800

    Complete RemoveShadow function

--- src/pyramid.py
+++ src/pyramid.py
@@ -1,21 +1,41 @@
 import cv2
+import numpy as np
+import copy
 
 def RemoveShadow(originalpng, shadowpng): # get gray image
 	shadownum = []
 	illunum = []
-	for i, j, num in enumerate(originalpng):
-		
+	for (i, j), num in np.ndenumerate(originalpng):
+		if shadowpng[i][j] == 0:
+			#not shadow
+			illunum.append(originalpng[i][j])
+		else:
+			#shadow
+			shadownum.append(originalpng[i][j])
+	shadowstd = np.std(shadownum)
+	illustd = np.std(illunum)
+	lamida = illustd / shadowstd
+	alpha = np.mean(illunum) - lamida * np.mean(shadownum)
+	res = copy.deepcopy(originalpng)
+	for (i, j), num in np.ndenumerate(originalpng):
+		if shadowpng[i][j] != 0:
+			#shadow
+			res[i][j] = alpha + lamida * res[i][j]
+	return res
 
-img = cv2.imread('../data/test.jpg',0) #
-img1 = cv2.pyrDown(img)#
+testimgname = '022'
+img = cv2.imread('../data/' + testimgname + '.jpg',0)
+shadowimg = cv2.imread('../data/' + testimgname + '.png', 0)
+img1 = cv2.pyrDown(img)
 temp_img1 = cv2.pyrDown(img1)
 temp = cv2.pyrUp(temp_img1)
 print img1
 print temp
-img2 = img1 - temp #
-cv2.imwrite('gauss.png', img1)
-for i in xrange(210):
+img2 = img1 - temp
+cv2.imwrite('ori.png', img)
+for i in xrange(168):
 	for j in xrange(img2[i].size):
 		img2[i][j] += 128
 print img2
-cv2.imwrite('lpls.png', img2)
+#cv2.imwrite('lpls.png', img2)
+cv2.imwrite('shadowremoval.png', RemoveShadow(img, shadowimg))
